45f6307a96593b62e5b27107b9a1ab5f24ba6c58
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-07 13:38:28 -0300

    feat: add weather model

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,29 @@
+class WeatherData {
+  final double? temp;
+  final double? tempMax;
+  final double? tempMin;
+  final String? descTemp;
+  final String? icon;
+  final int? humidity;
+
+  WeatherData(
+      {this.descTemp,
+      this.temp,
+      this.humidity,
+      this.icon,
+      this.tempMax,
+      this.tempMin});
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    String capitalize(String s) => '$s[0].toUpperCase$s.substring(1)';
+
+    return WeatherData(
+      temp: json['main']['temp'].toDouble(),
+      tempMax: json['main']['temp_max'].toDouble(),
+      tempMin: json['main']['temp_min'].toDouble(),
+      descTemp: capitalize(json['weather'][0]['description']),
+      icon: json['weather'][0]['icon'],
+      humidity: json['weather']['humidity'].toInt(),
+    );
+  }
+}
