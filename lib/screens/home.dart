9664ae9229a6c8d55b9e233a2078d853130746f6
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-07 16:41:19 -0300

    feat: implements http request

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/model/weather_model.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,8 +13,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late WeatherData weatherData;
+  bool isLoading = false;
   final List<String> _cities = ['São Paulo', 'London', 'New York'];
   String _selectedCity = 'São Paulo';
+
+  loadWeather() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    String apiKey = '1ee6499eee18e8165e749b2ce1d6d629';
+    String language = 'pt_br';
+    String units = 'metric';
+
+    Uri url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+      'appid': apiKey,
+      'q': _selectedCity,
+      'units': units,
+      'lang': language
+    });
+
+    final weatherResponse = await http.get(url);
+
+    if (weatherResponse.statusCode == 200) {
+      return setState(() {
+        weatherData = WeatherData.fromJson(jsonDecode(weatherResponse.body));
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
